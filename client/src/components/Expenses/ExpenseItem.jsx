abb933ad90ebc5ecea43f742c8310892d636f0fb
JM Baldonado <john.baldonado@xurpas.com>
2023-01-26 19:13:08 +0800

    lesson for composition and intro to useState

--- client/src/components/Expenses/ExpenseItem.jsx
+++ client/src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  const changeTitleHandler = () => {
+    setTitle("Updated");
+    console.log(title);
+  };
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
+    </Card>
   );
 };
 
